5271acc8823343d35415152a333061d17a322858
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-08-19 22:06:54 +0300

    add states and styles for adaptive menu burger

--- allegria-womens-clothing-store/src/Menu.js
+++ allegria-womens-clothing-store/src/Menu.js
@@ -11,59 +11,200 @@ import MenuDetailed from './MenuDetailed';
 class Menu extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { menuDetales: false }
+		this.state = {
+			menuDetales: false,
+			sizeWidthType: 'desktop',
+			openBurger: false,
+		}
+	}
+	getWindowDimensions = () => {
+		const { innerWidth: width } = window;
+		return width;
 	}
 
+	handleWindowDimensions = () => {
+		let width = this.getWindowDimensions()
+		if (width <= 375)
+			this.setState({ sizeWidthType: 'phone' });
+		else if (width <= 768)
+			this.setState({ sizeWidthType: 'tablet' });
+		else
+			this.setState({ sizeWidthType: 'desktop' });
+	}
+
+	componentDidMount() {
+		this.handleWindowDimensions();
+		window.addEventListener('resize', this.handleWindowDimensions);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleWindowDimensions);
+	}
 	render() {
-		return (
-			<header className='menu'>
-				<div className='containerM'>
-					<div className='menu__body'>
-						<div className='menu__logo'>
-							<img src={a} alt="logo" />
-							<img src={l} alt="logo" />
-							<img src={l} alt="logo" />
-							<img src={e} alt="logo" />
-							<img src={g} alt="logo" />
-							<img src={r} alt="logo" />
-							<img src={i} alt="logo" />
-							<img src={a} alt="logo" />
-						</div>
-						<div className='menu__burger'>
-							<span></span>
-						</div>
-						<nav className='menu__navigate'>
-							<ul>
-								<li onClick={() => this.setState({ menuDetales: !this.state.menuDetales })}
-									className={((this.state.menuDetales === true) ? 'menu__navigate-active' : 'menu__navigate-women')}>
-									Женщины
-								</li>
-								<li>Мужчины</li>
-								<li className='menu__navigate-search'>
-									поиск
-									<input className='menu__navigate-search-input' type='text'></input>
-								</li>
-							</ul>
-						</nav>
-						<div className='menu__lang'>
-							<ul>
-								<li className='menu__lang-active'>ru</li>
-								<li>ua</li>
-								<li>en</li>
-							</ul>
-						</div>
-						<div className='menu__icons'>
-							<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
-							<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
-							<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
-						</div>
-						<div className={((this.state.menuDetales === true) ? 'munu__detailed munu__detailed-active' : 'munu__detailed')}>
-							<MenuDetailed />
+		// const { sizeWidthType } = this.state;
+		switch (this.state.sizeWidthType) {
+			case 'desktop': return (
+				<header className='menu'>
+					<div className='containerM'>
+						<div className='menu__body'>
+							<div className='menu__logo'>
+								<img src={a} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={e} alt="logo" />
+								<img src={g} alt="logo" />
+								<img src={r} alt="logo" />
+								<img src={i} alt="logo" />
+								<img src={a} alt="logo" />
+							</div>
+							<nav className='menu__navigate'>
+								<ul>
+									<li onClick={() => this.setState({ menuDetales: !this.state.menuDetales })}
+										className={((this.state.menuDetales === true) ? 'menu__navigate-active' : 'menu__navigate-women')}>
+										Женщины
+									</li>
+									<li>Мужчины</li>
+									<li className='menu__navigate-search'>
+										поиск
+										<input className='menu__navigate-search-input' type='text'></input>
+									</li>
+								</ul>
+							</nav>
+							<div className='menu__lang'>
+								<ul>
+									<li className='menu__lang-active'>ru</li>
+									<li>ua</li>
+									<li>en</li>
+								</ul>
+							</div>
+							<div className='menu__icons'>
+								<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
+								<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
+								<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
+							</div>
+							<div className={((this.state.menuDetales === true) ? 'menu__detailed menu__detailed-active' : 'menu__detailed')}>
+								<MenuDetailed openAccordion={true} />
+							</div>
 						</div>
 					</div>
-				</div>
-			</header>
-		)
+				</header>
+			);
+			case 'tablet': return (
+				<header className='menu'>
+					<div className='containerM'>
+						<div className='menu__body'>
+							<div className='menu__logo'>
+								<img src={a} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={e} alt="logo" />
+								<img src={g} alt="logo" />
+								<img src={r} alt="logo" />
+								<img src={i} alt="logo" />
+								<img src={a} alt="logo" />
+							</div>
+							<div className='menu__phone-right'>
+								<div className='menu__icons'>
+									<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
+									<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
+									<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
+								</div>
+								<div className='menu__burger' onClick={() => this.setState({ openBurger: !this.state.openBurger })}>
+									{(this.state.openBurger === false)
+										?
+										<i className="bi bi-filter-right" style={{ fontSize: '25px' }}></i>
+										: <i className="bi bi-x-lg" style={{ fontSize: '23px' }}></i>
+									}
+								</div>
+							</div>
+							<div className={((this.state.openBurger === true) ? 'menu__detailed menu__detailed-active' : 'menu__detailed')}>
+								<div className='menu__detailed-table'>
+									<nav className='menu__navigate'>
+										<ul>
+											<li className='menu__navigate-active'>
+												Женщины
+											</li>
+											<li>Мужчины</li>
+											<li className='menu__navigate-search-table'>
+												поиск
+												<input type='text'></input>
+											</li>
+										</ul>
+									</nav>
+									<div className='menu__lang'>
+										<ul>
+											<li className='menu__lang-active'>ru</li>
+											<li>ua</li>
+											<li>en</li>
+										</ul>
+									</div>
+								</div>
+								<MenuDetailed openAccordion={false} />
+							</div>
+						</div>
+					</div>
+				</header>
+			);
+			case 'phone': return (
+				<header className='menu'>
+					<div className='containerM'>
+						<div className='menu__body'>
+							<div className='menu__logo'>
+								<img src={a} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={l} alt="logo" />
+								<img src={e} alt="logo" />
+								<img src={g} alt="logo" />
+								<img src={r} alt="logo" />
+								<img src={i} alt="logo" />
+								<img src={a} alt="logo" />
+							</div>
+							<div className='menu__phone-right'>
+								<div className='menu__icons'>
+									<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
+									<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
+									<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
+								</div>
+								<div className='menu__burger' onClick={() => this.setState({ openBurger: !this.state.openBurger })}>
+									{(this.state.openBurger === false)
+										?
+										<i className="bi bi-filter-right" style={{ fontSize: '25px' }}></i>
+										: <i className="bi bi-x-lg" style={{ fontSize: '23px' }}></i>
+									}
+								</div>
+							</div>
+							<div className={((this.state.openBurger === true) ? 'menu__detailed menu__detailed-active' : 'menu__detailed')}>
+								<div className='menu__detailed-table'>
+									<nav className='menu__navigate'>
+										<ul>
+											<li className='menu__navigate-active'>
+												Женщины
+											</li>
+											<li>Мужчины</li>
+
+										</ul>
+									</nav>
+								</div>
+								<MenuDetailed openAccordion={false} />
+								<div className='menu__lang'>
+									<ul className='menu__lang-phone-ul'>
+										<li className='menu__navigate-search-phone'>
+											поиск
+											<input type='text'></input>
+										</li>
+										<li className='menu__lang-active'>ru</li>
+										<li>ua</li>
+										<li>en</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</header>
+			)
+
+			default: break;
+		}
 	}
 }
 
--- allegria-womens-clothing-store/src/MenuDetailed.js
+++ allegria-womens-clothing-store/src/MenuDetailed.js
@@ -6,93 +6,101 @@ import { Accordion } from 'react-bootstrap';
 class MenuDetailed extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { menuDetales: false }
+		this.state = {}
 	}
 
 	render() {
 		return (
-			<div className='menu-detailed'>
-				<div className='menu-detailed__clothes'>
-					<Accordion alwaysOpen>
-						<Accordion.Item>
-							<Accordion.Header>ОДЕЖДА</Accordion.Header>
-							<Accordion.Body>
-								<ul>
-									<li>Свитшоты и худи</li>
-									<li>Платья юбки</li>
-									<li>Футболки и топы</li>
-									<li>Брюки и шорты</li>
-									<li>Рубашки</li>
-									<li>Комбинезоны</li>
-									<li>Леггинсы</li>
-									<li>Куртки</li>
-									<li>Пальто</li>
-									<li>Плащи</li>
-									<li>Желетки</li>
-									<li>Ветровки</li>
-								</ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
+			<div className={((this.props.openAccordion === true) ? 'menu-detailed' : 'menu-detailed__tablet')}>
+				<div className={((this.props.openAccordion === true) ? 'menu-detailed__choise' : ' menu-detailed__phone')}>
+					<div className='menu-detailed__clothes'>
+						<Accordion defaultActiveKey={(this.props.openAccordion === true) ? '0' : ''}>
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>ОДЕЖДА</Accordion.Header>
+								<Accordion.Body style={(this.props.openAccordion !== true) ? { 'text-align': 'center' } : {}}>
+									<ul>
+										<li>Свитшоты и худи</li>
+										<li>Платья юбки</li>
+										<li>Футболки и топы</li>
+										<li>Брюки и шорты</li>
+										<li>Рубашки</li>
+										<li>Комбинезоны</li>
+										<li>Леггинсы</li>
+										<li>Куртки</li>
+										<li>Пальто</li>
+										<li>Плащи</li>
+										<li>Желетки</li>
+										<li>Ветровки</li>
+									</ul>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='menu-detailed__shoes'>
+						<Accordion defaultActiveKey={(this.props.openAccordion === true) ? '0' : ''}>
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>ОБУВЬ</Accordion.Header>
+								<Accordion.Body style={(this.props.openAccordion !== true) ? { 'text-align': 'center' } : {}}>
+									<ul>
+										<li>Кроссовки</li>
+										<li>Шлепанцы</li>
+									</ul>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='menu-detailed__bags'>
+						<Accordion defaultActiveKey={(this.props.openAccordion === true) ? '0' : ''}>
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>СУМКИ</Accordion.Header>
+								<Accordion.Body style={(this.props.openAccordion !== true) ? { 'text-align': 'center' } : {}}>
+									<ul>
+										<li>Сумки</li>
+										<li>Рюкзаки</li>
+										<li>Кроссбоди</li>
+										<li>Поясные</li>
+										<li>Спортивные</li>
+										<li>Шопперы</li>
+									</ul>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='menu-detailed__accessories'>
+						<Accordion defaultActiveKey={(this.props.openAccordion === true) ? '0' : ''}>
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>АКСЕСУАРИ</Accordion.Header>
+								<Accordion.Body style={(this.props.openAccordion !== true) ? { 'text-align': 'center' } : {}}>
+									<ul>
+										<li>Головные уборы</li>
+										<li>Перчатки</li>
+										<li>Шарфы и платки</li>
+										<li>Носки </li>
+										<li>Гетры</li>
+										<li>Др</li>
+									</ul>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='menu-detailed__underwear'>
+						<Accordion defaultActiveKey={(this.props.openAccordion === true) ? '0' : ''}>
+							<Accordion.Item eventKey="0">
+								<Accordion.Header>БЕЛЬЕ</Accordion.Header>
+								<Accordion.Body style={(this.props.openAccordion !== true) ? { 'text-align': 'center' } : {}}>
+									<ul>
+										<li>Разное</li>
+									</ul>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
 				</div>
-				<div className='menu-detailed__shoes'>
-					<Accordion alwaysOpen>
-						<Accordion.Item>
-							<Accordion.Header>ОБУВЬ</Accordion.Header>
-							<Accordion.Body>
-								<ul>
-									<li>Кроссовки</li>
-									<li>Шлепанцы</li>
-								</ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
-				</div>
-				<div className='menu-detailed__bags'>
-					<Accordion alwaysOpen>
-						<Accordion.Item>
-							<Accordion.Header>СУМКИ</Accordion.Header>
-							<Accordion.Body>
-								<ul>
-									<li>Сумки</li>
-									<li>Рюкзаки</li>
-									<li>Кроссбоди</li>
-									<li>Поясные</li>
-									<li>Спортивные</li>
-									<li>Шопперы</li>
-								</ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
-				</div>
-				<div className='menu-detailed__accessories'>
-					<Accordion alwaysOpen>
-						<Accordion.Item>
-							<Accordion.Header>АКСЕСУАРИ</Accordion.Header>
-							<Accordion.Body>
-								<ul>
-									<li>Головные уборы</li>
-									<li>Перчатки</li>
-									<li>Шарфы и платки</li>
-									<li>Носки </li>
-									<li>Гетры</li>
-									<li>Др</li>
-								</ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
-				</div>
-				<div className='menu-detailed__underwear'>
-					<Accordion alwaysOpen>
-						<Accordion.Item>
-							<Accordion.Header>БЕЛЬЕ</Accordion.Header>
-							<Accordion.Body>
-								<ul>
-									<li>Разное</li>
-								</ul>
-							</Accordion.Body>
-						</Accordion.Item>
-					</Accordion>
+				<div className='menu-detailed__info'>
+					<span>DEHA</span>
+					<span>American vintage</span>
+					<span>george gina lucy</span>
+					<span>george gina lucy</span>
 				</div>
 			</div>
 		)
