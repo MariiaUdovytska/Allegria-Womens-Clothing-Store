67d84e4e8669dfe3b4875630d6d5218f5e28fec1
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-08-19 12:43:11 +0300

    add state and styles for invoked menuDetailes

--- allegria-womens-clothing-store/src/Menu.js
+++ allegria-womens-clothing-store/src/Menu.js
@@ -6,50 +6,63 @@ import e from './image/logo/e.png';
 import g from './image/logo/g.png';
 import r from './image/logo/r.png';
 import i from './image/logo/i.png';
+import MenuDetailed from './MenuDetailed';
 
 class Menu extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { menuDetales: false }
 	}
+
 	render() {
 		return (
-			<div className='menu containerM'>
-				<div className='menu__logo'>
-					<img src={a} alt="logo" />
-					<img src={l} alt="logo" />
-					<img src={l} alt="logo" />
-					<img src={e} alt="logo" />
-					<img src={g} alt="logo" />
-					<img src={r} alt="logo" />
-					<img src={i} alt="logo" />
-					<img src={a} alt="logo" />
+			<header className='menu'>
+				<div className='containerM'>
+					<div className='menu__body'>
+						<div className='menu__logo'>
+							<img src={a} alt="logo" />
+							<img src={l} alt="logo" />
+							<img src={l} alt="logo" />
+							<img src={e} alt="logo" />
+							<img src={g} alt="logo" />
+							<img src={r} alt="logo" />
+							<img src={i} alt="logo" />
+							<img src={a} alt="logo" />
+						</div>
+						<div className='menu__burger'>
+							<span></span>
+						</div>
+						<nav className='menu__navigate'>
+							<ul>
+								<li onClick={() => this.setState({ menuDetales: !this.state.menuDetales })}
+									className={((this.state.menuDetales === true) ? 'menu__navigate-active' : 'menu__navigate-women')}>
+									Женщины
+								</li>
+								<li>Мужчины</li>
+								<li className='menu__navigate-search'>
+									поиск
+									<input className='menu__navigate-search-input' type='text'></input>
+								</li>
+							</ul>
+						</nav>
+						<div className='menu__lang'>
+							<ul>
+								<li className='menu__lang-active'>ru</li>
+								<li>ua</li>
+								<li>en</li>
+							</ul>
+						</div>
+						<div className='menu__icons'>
+							<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
+							<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
+							<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
+						</div>
+						<div className={((this.state.menuDetales === true) ? 'munu__detailed munu__detailed-active' : 'munu__detailed')}>
+							<MenuDetailed />
+						</div>
+					</div>
 				</div>
-				<nav className='menu__navigate'>
-					<ul>
-						<li>Женщины</li>
-						<li>Мужчины</li>
-						<li className='menu__navigate-search'>
-							поиск
-							<input className='menu__navigate-search-input' type='text'></input>
-						</li>
-					</ul>
-				</nav>
-				<div className='menu__lang'>
-					<ul>
-						<li className='menu__lang-active'>ru</li>
-						<li>ua</li>
-						<li>en</li>
-					</ul>
-				</div>
-				<div className='menu__icons'>
-					<i className="bi bi-person" style={{ fontSize: '23px' }}></i>
-					<i className="bi bi-heart" style={{ fontSize: '19px' }}></i>
-					<i className="bi bi-bag" style={{ fontSize: '20px' }}></i>
-				</div>
-				<div className='menu__burger'>
-					<span></span>
-				</div>
-			</div>
+			</header>
 		)
 	}
 }
---
+++ allegria-womens-clothing-store/src/MenuDetailed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/menuDetailed.css';
+
+class MenuDetailed extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { menuDetales: false }
+	}
+
+	render() {
+		return (
+			<div className='menu-detailed'>
+				test
+			</div>
+		)
+	}
+}
+
+export default MenuDetailed;
