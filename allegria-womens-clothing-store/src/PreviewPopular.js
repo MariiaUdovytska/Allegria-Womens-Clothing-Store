3f1e07ed1d09a383aac92d90bb5881e2429c9d90
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-09-10 17:10:41 +0300

    added a tracker to change the number of visible Зкщв-cards

--- allegria-womens-clothing-store/src/PreviewPopular.js
+++ allegria-womens-clothing-store/src/PreviewPopular.js
@@ -9,12 +9,45 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper";
 import ProductCard from './ProductCard';
 
+import a from './image/logo/a.png';
+import l from './image/logo/l.png';
+import e from './image/logo/e.png';
+import g from './image/logo/g.png';
+import r from './image/logo/r.png';
+import i from './image/logo/i.png';
+
 class PreviewPopular extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			sizeWidthType: 'desktop',
+		}
+	}
+	getWindowDimensions = () => {
+		const { innerWidth: width } = window;
+		return width;
 	}
 
+	handleWindowDimensions = () => {
+		let width = this.getWindowDimensions()
+		if (width <= 502)
+			this.setState({ sizeWidthType: 'phone' });
+		else if (width <= 768)
+			this.setState({ sizeWidthType: 'tablet' });
+		else
+			this.setState({ sizeWidthType: 'desktop' });
+	}
+
+	componentDidMount() {
+		this.handleWindowDimensions();
+		window.addEventListener('resize', this.handleWindowDimensions);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleWindowDimensions);
+	}
 	render() {
+
 		let dataCard = stockCards;
 		let arrayLi = [];
 		for (let index = 0; index < dataCard.length; index++) {
@@ -31,13 +64,27 @@ class PreviewPopular extends React.Component {
 					/>
 				</SwiperSlide>);
 		}
+		let cardsNum = 4;
+		switch (this.state.sizeWidthType) {
+			case 'desktop': cardsNum = 4; break;
+			case 'tablet': cardsNum = 3; break;
+			case 'phone': cardsNum = 2; break;
+			default: break;
+		}
+		let spaceBetween = 30;
+		switch (this.state.sizeWidthType) {
+			case 'desktop': spaceBetween = 30; break;
+			case 'tablet': spaceBetween = 20; break;
+			case 'phone': spaceBetween = 15; break;
+			default: break;
+		}
 		return (
 			<div className='preview-popular'>
 				<div className='preview-popular__body'>
-					<h2>Популярное</h2>
+					<h2>Популярне</h2>
 					<Swiper
-						slidesPerView={4}
-						spaceBetween={30}
+						slidesPerView={cardsNum}
+						spaceBetween={spaceBetween}
 						pagination={{
 							clickable: true
 						}}
@@ -46,6 +93,18 @@ class PreviewPopular extends React.Component {
 					>
 						{arrayLi}
 					</Swiper>
+					<div className='preview-popular__body-tablet'>
+						<div className='preview-popular__body-tablet-logo'>
+							<img src={a} alt="logo" />
+							<img src={l} alt="logo" />
+							<img src={l} alt="logo" />
+							<img src={e} alt="logo" />
+							<img src={g} alt="logo" />
+							<img src={r} alt="logo" />
+							<img src={i} alt="logo" />
+							<img src={a} alt="logo" />
+						</div>
+					</div>
 				</div>
 			</div>
 		)
