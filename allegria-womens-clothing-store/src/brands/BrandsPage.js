c789d13f3592804563501197089c7cac6e8691be
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-09-15 17:36:10 +0300

    add styles for BradsPage

--- allegria-womens-clothing-store/src/brands/BrandsPage.js
+++ allegria-womens-clothing-store/src/brands/BrandsPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../css/brands/brands.css';
 // import image32 from '../image/aboutPage/image32.png';
-// import image62 from '../image/aboutPage/image62.png';
-// import outlineElipse from '../image/outlineElipse.png';
+import image62 from '../image/brandsPage/image62.png';
+import image25 from '../image/brandsPage/image25.png';
+import feets from '../image/brandsPage/4feets.png';
+import outlineElipse8 from '../image/outlineElipse8.png';
 
 function BrandsPage() {
 	return (
@@ -16,7 +18,18 @@ function BrandsPage() {
 							<p>American Vintage - модная торговая марка по производству трикотажной одежды для мужчин и женщин. </p>
 						</div>
 					</div>
-					<div>test test </div>
+					<div className='brands__body-info info1'>
+						<div className='brands__body-info-up'>
+							<div className='brands__body-info-up-img info-img1'></div>
+							<div className='brands__body-info-up-text up-text1'>
+								<h5>american vintage</h5>
+								<p>Продукция также включает немногочисленные аксессуары в виде шарфов, ремней, шапок, а также женских сумок.<br></br>Несмотря на название «American Vintage», бренд создан французом Мишель Азулэ.<br></br>Отличие бренда от других - это простота в исполнении изделий (минимализм) и  100% натуральные ткани. бренд сочетает в себе расслабленность южной Франции и простоту Америки.  При создании одежды используется очень тонкий высококачественный трикотаж, который дает ощущение «второй кожи». Основная цветовая гамма - это спокойные пастельные расцветки: ореховые, бежевые, желтые, серые и так далее.<br></br>На первый взгляд, кажется, что вещи просты в своем исполнении, но вся продукция от этой марки выглядит очень  стильно, оставляя без ума многих покупателей.</p>
+							</div>
+						</div>
+						<div className='brands__body-info-down1'>
+							<img src={image62} alt='gerl'></img>
+						</div>
+					</div>
 				</section>
 				<section>
 					<div className='brands__body-full full-screen2'>
@@ -26,9 +39,23 @@ function BrandsPage() {
 							<p>Бренд DEHA принадлежит компании Meeting Group SpA, которая является лидером на итальянском рынке спортивной одежды премиум-класса. </p>
 						</div>
 					</div>
-					<div>test test
-						<p>test test </p>
-						<p>test test </p>
+					<div className='brands__body-info info2'>
+						<div className='brands__body-info-up'>
+							<div className='brands__body-info-up-text up-text2'>
+								<h5>DeHA</h5>
+								<p>Требование и суть коллекций DEHA-это  чувство женственности ,  не показной, но подходящей  всем женщинам, которые являются женами, матерями, профессионалами, сексуальными, спортивными и веселыми одновременно.<br></br>Коллекции DEHA идеально подходят для ежедневных тренировок, но Deha – больше, чем одежда для спорта, это удобная и естественная женственность на каждый день.Это яркие струящиеся ткани, легкость и удобство.<br></br>DEHA очень чутко относится к теме экологической устойчивости  и защите окружающей среды. Поэтому расширяет каждый сезон ассортимент одежды из органических натуральных волокон и переработанных материалов, вносит свой вклад в  благосостояние планеты . </p>
+							</div>
+							<div className='brands__body-info-up-img info-img2'></div>
+						</div>
+						<div className='brands__body-info-down2'>
+							<div className='brands__body-info-down2-combiantion'>
+								<img src={image25} alt='gerl'></img>
+								<div className='brands__body-info-down2-forms'>
+									<span></span>
+									<img src={outlineElipse8} alt="outline-elipse"></img>
+								</div>
+							</div>
+						</div>
 					</div>
 				</section>
 				<section>
@@ -38,8 +65,14 @@ function BrandsPage() {
 						<p>Бренд основан в 2004 году недалеко от Франкфурта, когда супруги Николь Бейли и Николас Нойхаус вместе с Оливером Брюном выпустили свою первую и очень удачную коллекцию сумок, каждая из которых имела свое имя.  </p>
 					</div>
 					<div className='brands__body-full full-screen3'></div>
-					<div>test test
-						<p>test test </p>
+					<div className='brands__body-info info3'>
+						<div className='brands__body-info-up'>
+							<div className='brands__body-info-up-img info-img3'></div>
+							<div className='brands__body-info-up-text up-text3'>
+								<h5>george gina & lucy</h5>
+								<p>Популярность и любовь дизайнеры заслужили тем, что соединили в своих творениях оригинальность, индивидуальность и легкий юмор, что вызвало у них появление огромного количества поклонниц во всем мире.<br></br>Каждая сумка этого уникального бренда неповторима и индивидуальна, а их отличие от других брендовых сумок в том, что они имеют преувеличенно крупную фурнитуру – кольца, карабины и молнии.<br></br>Такие сумки созданы специально для тех , кто любит  индивидуальность и неповторимость.</p>
+							</div>
+						</div>
 					</div>
 				</section>
 				<section>
@@ -50,8 +83,23 @@ function BrandsPage() {
 							<p>BIRKENSTOCK — известная немецкая компания, занимающаяся производством ортопедической обуви. </p>
 						</div>
 					</div>
-					<div>test test
-						<p>test test </p>
+					<div className='brands__body-info info2'>
+						<div className='brands__body-info-up'>
+							<div className='brands__body-info-up-text up-text2'>
+								<h5>birkenstock</h5>
+								<p>Свою славу она заслужила благодаря удобству, экологичности материалов и поддержанию здоровья ног. Знаменитая мягкая ортопедическая стелька фирмы BIRKENSTOCK позволяет долгое время находиться в обуви не уставая.<br></br>В 1990 году пара сандалий BIRKENSTOCK Аризона в США была признана самой экологичной.<br></br>В 2004 году BIRKENSTOCK были удостоены звания «лучший дизайн, проверенный годами и подходящий всем».<br></br>На данном этапе бренд производит не только вьетнамки и сандалии, а и много других комфортных видов обуви. Созданы в стиле кэжуал и вписываются в будничные образы. Бренд также прославился капсульными коллекциями, выпущенными в коллаборации со многими современными дизайнерами.</p>
+							</div>
+							<div className='brands__body-info-up-img info-img4'></div>
+						</div>
+						<div className='brands__body-info-down2'>
+							<div className='brands__body-info-down2-combiantion'>
+								<img src={feets} alt='gerl'></img>
+								<div className='brands__body-info-down2-forms'>
+									<span></span>
+									<img src={outlineElipse8} alt="outline-elipse"></img>
+								</div>
+							</div>
+						</div>
 					</div>
 				</section>
 			</div>
