dccb8ce9eb5ddcbda000200a49b54d1e57f0ecf4
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-09-22 11:24:10 +0300

    add component and styles for SortProducts

--- allegria-womens-clothing-store/src/productCardsPage/SortProducts.js
+++ allegria-womens-clothing-store/src/productCardsPage/SortProducts.js
@@ -1,11 +1,19 @@
-
 import React from 'react';
 import '../css/productCardsPage/sortProducts.css';
+import { Form } from 'react-bootstrap';
 
 function SortProducts() {
 	return (
 		<div className='sort'>
-			test sort
+			<span>Соритировать:</span>
+			<div className='sort__accordion menu-detailed__accordion'>
+				<Form.Select aria-label="Новинки">
+					<option>Новинки</option>
+					<option value="1">По возрастанию цены</option>
+					<option value="2">По убыванию цены</option>
+				</Form.Select>
+			</div>
+
 		</div>
 	);
 }
