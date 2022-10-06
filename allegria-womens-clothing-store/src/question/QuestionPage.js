1b9af437e3a526100369d26298adb98e142e8d64
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-10-06 20:51:27 +0300

    add accordions questions and styles reed side

--- allegria-womens-clothing-store/src/question/QuestionPage.js
+++ allegria-womens-clothing-store/src/question/QuestionPage.js
@@ -8,7 +8,7 @@ function QuestionPage() {
 			<h1>Популярные вопросы</h1>
 			<div className='question__body'>
 				<div className='question__body-reed'>
-					<h2>ПОКУПКИ</h2>
+					<h2 className=''>ПОкупкИ</h2>
 					<div className='question__body-reed-accordion'>
 						<Accordion defaultActiveKey="0">
 							<Accordion.Item eventKey="0">
@@ -71,6 +71,36 @@ function QuestionPage() {
 					</div>
 
 					<h2>ВОЗВРАТ И ОБМЕН</h2>
+					<div className='question__body-reed-accordion'>
+						<Accordion defaultActiveKey="0">
+							<Accordion.Item eventKey="1">
+								<Accordion.Header>Вопрос от покупателя</Accordion.Header>
+								<Accordion.Body >
+									<p>Оплата осуществляется наличными денежными средствами курьеру непосредственно при доставке заказа на адрес получателя. Выбрав данный способ оплаты, Вы получаете возможность рассмотреть и примерить заказанный товар.<br></br>Если какая-то позиция Вам не подошла, Вы можете просто вернуть ее курьеру, не оплачивая.</p>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='question__body-reed-accordion'>
+						<Accordion defaultActiveKey="0">
+							<Accordion.Item eventKey="1">
+								<Accordion.Header>Вопрос от покупателя</Accordion.Header>
+								<Accordion.Body >
+									<p>Оплата осуществляется наличными денежными средствами курьеру непосредственно при доставке заказа на адрес получателя. Выбрав данный способ оплаты, Вы получаете возможность рассмотреть и примерить заказанный товар.<br></br>Если какая-то позиция Вам не подошла, Вы можете просто вернуть ее курьеру, не оплачивая.</p>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
+					<div className='question__body-reed-accordion'>
+						<Accordion defaultActiveKey="0">
+							<Accordion.Item eventKey="1">
+								<Accordion.Header>Вопрос от покупателя</Accordion.Header>
+								<Accordion.Body >
+									<p>Оплата осуществляется наличными денежными средствами курьеру непосредственно при доставке заказа на адрес получателя. Выбрав данный способ оплаты, Вы получаете возможность рассмотреть и примерить заказанный товар.<br></br>Если какая-то позиция Вам не подошла, Вы можете просто вернуть ее курьеру, не оплачивая.</p>
+								</Accordion.Body>
+							</Accordion.Item>
+						</Accordion>
+					</div>
 				</div>
 				<div className='question__body-write'>test write</div>
 			</div>
