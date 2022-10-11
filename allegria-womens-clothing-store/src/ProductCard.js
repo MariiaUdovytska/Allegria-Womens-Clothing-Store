091158fb6d3696599a80851b4b0970716e7a2cf0
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-10-11 18:18:49 +0300

    change what need click for go to prodPage, add ternar handleClickLike for change color heart

--- allegria-womens-clothing-store/src/ProductCard.js
+++ allegria-womens-clothing-store/src/ProductCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './css/productCard.css';
 import { Link } from 'react-router-dom';
 
@@ -6,24 +6,35 @@ function ProductCard(props) {
 	let showBusket = props.price !== undefined;
 	let showSale = props.sale !== undefined;
 
+	const [liked, setLiked] = useState(false);
+
+	let handleClickLike = () => {
+		setLiked(true);
+	}
+
 	let link = `/product?id=${props.id}`;
 	if (props.id === undefined) {
 		link = '/';
 	}
 	return (
 		<div className='router-link'>
-			<Link to={link}>
-				<div className='cardd'>
-					<div className='card__body'>
-						<span className='card__body-hover'><span>переглянути</span></span>
-						<div className='card__body-img'>
-							<img src={props.image} alt='imageProduct'></img>
-							<div className='card__body-icons'>
-								<div className='card__body-up-like'><i className='bi bi-heart-fill' style={{ fontSize: '16px', color: '#E64926' }}></i></div>
-								<div className='card__body-up-like dopempty'><i className='bi bi-heart' style={{ fontSize: '16px' }}></i></div>
-								<div className='card__body-up-sale' style={{ visibility: (showSale === true ? 'visible' : 'hidden') }}><span>{props.sale} %</span></div>
-							</div>
+			<div className='cardd'>
+				<div className='card__body'>
+					<Link to={link}><span className='card__body-hover'><span>переглянути</span></span></Link>
+					<div className='card__body-img'>
+						<Link to={link}><img src={props.image} alt='imageProduct'></img></Link>
+						<div onClick={() => handleClickLike()} className='card__body-icons'>
+							{liked === true
+								? <div className='card__body-up-like'>
+									<i className='bi bi-heart-fill' style={{ fontSize: '16px', color: '#E64926' }}></i></div>
+								: <div className='card__body-up-like'>
+									<i className='bi bi-heart' style={{ fontSize: '16px' }}></i>
+								</div>
+							}
+							<div className='card__body-up-sale' style={{ visibility: (showSale === true ? 'visible' : 'hidden') }}><span>{props.sale} %</span></div>
 						</div>
+					</div>
+					<Link to={link}>
 						<div className='card__body-nameprice'>
 							<h4>{props.name}</h4>
 							<h5>{props.categories}</h5>
@@ -37,11 +48,10 @@ function ProductCard(props) {
 								</div>
 							}
 						</div>
-					</div>
+					</Link>
 				</div>
-			</Link>
+			</div>
 		</div>
-
 	)
 }
 
