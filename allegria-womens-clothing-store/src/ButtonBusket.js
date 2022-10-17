3f42a6ca3916da6dc72c75b6663a515779382c17
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-10-17 16:04:57 +0300

    add ButtonBusket with logic(useState inBusket->disabled; show Toas)

--- allegria-womens-clothing-store/src/ButtonBusket.js
+++ allegria-womens-clothing-store/src/ButtonBusket.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/wishlist/wishlist.css';
+import Toast from 'react-bootstrap/Toast';
 
 function getIds() {
 	let idsArray = localStorage.getItem("idsBusket");
@@ -11,6 +12,8 @@ function getIds() {
 }
 
 function ButtonBusket(props) {
+	const [toastShow, setToastShow] = useState(false);
+	const [inBusket, setInBusket] = useState(getIds().includes(props.id));
 
 	let handleClickBusket = () => {
 		if (props.id == undefined)
@@ -20,11 +23,28 @@ function ButtonBusket(props) {
 		if (!ids_l.includes(props.id)) {
 			ids_l.push(props.id);
 			localStorage.setItem("idsBusket", JSON.stringify(ids_l));
+			setToastShow(true);
+			setInBusket(true);
 		}
 	}
 
 	return (
-		<button onClick={() => handleClickBusket()} className='wishlist__body-cards-info-right-btn' type='submit'>добавить в корзину</button>
+		<>
+			<button
+				onClick={() => handleClickBusket()}
+				className='wishlist__body-cards-info-right-btn'
+				type='submit'
+				disabled={inBusket || props.disabled}
+			>
+				добавить в корзину
+			</button>
+			<Toast onClose={() => setToastShow(false)} show={toastShow} delay={1500} autohide style={{ position: "fixed", bottom: "25px", right: "25px" }}>
+				<Toast.Header closeButton={false}>
+					<strong className="me-auto">Allegria</strong>
+				</Toast.Header>
+				<Toast.Body>Добавленно в корзину</Toast.Body>
+			</Toast>
+		</>
 	)
 }
 
