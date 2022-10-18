ef93acf4908a6555abd216b8d6e3e5e7314dc8e0
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-10-18 23:09:38 +0300

    add touchMove to right or top for Filter Offcanvas in cardSPage

--- allegria-womens-clothing-store/src/productCardsPage/OffCanvas.js
+++ allegria-womens-clothing-store/src/productCardsPage/OffCanvas.js
@@ -6,20 +6,63 @@ import ExtensionFilter from './ExtensionFilter';
 
 function OffCanvas({ name, buttonName, ...props }) {
 	const [show, setShow] = useState(false);
+	const [touchPosition, setTouchPosition] = useState(null);
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
+	let handleTouchStart = (e) => {
+		let touchDown = 0;
+		switch (props.placement) {
+			case 'top':
+				touchDown = e.touches[0].clientY;
+				setTouchPosition(touchDown);
+				break;
+			case 'end':
+				touchDown = e.touches[0].clientX;
+				setTouchPosition(touchDown);
+				break;
+			default: break;
+		}
+	}
+
+	let handleTouchMove = (e) => {
+		const touchDown = touchPosition
+		if (touchDown === null) {
+			return
+		}
+		if (props.placement === 'top') {
+			const currentTouch = e.touches[0].clientY
+			const diff = touchDown - currentTouch
+			if (diff > 5) {
+				setShow(false);
+			}
+			setTouchPosition(null);
+		} else {
+			const currentTouch = e.touches[0].clientX
+			const diff = touchDown - currentTouch
+			if (diff < 5) {
+				setShow(false);
+			}
+			setTouchPosition(null);
+		}
+	}
+
 	return (
 		<>
-			<Button variant="primary" onClick={handleShow} className="me-2">
+			<Button
+				variant="primary"
+				onClick={handleShow}
+				className="me-2">
 				{buttonName}
 			</Button>
-			<Offcanvas show={show} onHide={handleClose} {...props} className='offcanvas-filter' >
+			<Offcanvas show={show} onHide={handleClose} {...props} className='offcanvas-filter'>
 				<Offcanvas.Header closeButton>
 					<Offcanvas.Title>Фильтр</Offcanvas.Title>
 				</Offcanvas.Header>
-				<Offcanvas.Body>
+				<Offcanvas.Body
+					onTouchStart={handleTouchStart}
+					onTouchMove={handleTouchMove}>
 					<FilterProdcts openAccordion={false} />
 					<ExtensionFilter />
 				</Offcanvas.Body>
