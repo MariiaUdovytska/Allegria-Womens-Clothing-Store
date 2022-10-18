44892c5d03c2e54668161be7b14b2eea67f0d462
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-10-18 11:31:52 +0300

    add link go too productCards from emptyBasket and adaptive

--- allegria-womens-clothing-store/src/busket/BasketIsEmpty.js
+++ allegria-womens-clothing-store/src/busket/BasketIsEmpty.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/busket/basketIsEmpty.css';
 import basket from '../image/basket.png';
 import shadowEllipse from '../image/shadowEllipse.png';
@@ -7,14 +8,14 @@ function BasketIsEmpty(props) {
 
 	return (
 		<div className='busket-empty containerM'>
-			<div className='busket-empty__body'>
+			<div className='busket-empty__body router-link'>
 				<h3>Ваша корзина пуста</h3>
 				<div className='busket-empty__body-imgs'>
 					<img src={basket} alt='basket is empty'></img>
 					<img src={shadowEllipse} alt='basket is empty'></img>
 				</div>
 				<p>Добавьте что-то, чтобы сэкономить время и сделать <br></br>шопинг еще более приятным.</p>
-				<button type='button'>перейти в каталог</button>
+				<Link to='/productCardsPage'><button type='button'>перейти в каталог</button></Link>
 			</div>
 		</div>
 	);
